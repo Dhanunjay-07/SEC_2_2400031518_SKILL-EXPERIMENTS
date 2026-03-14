f60195cd6df13cda05fe64d4b088557e58a9c383
Dhanunjay-07 <2400031518@kluniversity.in>
2026-03-14 20:35:34 +0530

    Initial Commit

--- SKILL-1/main.java
+++ SKILL-1/main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Bug fixed version of Git experiment");
+
+        System.out.println("FSAD Skill Experiment 1");
+        System.out.println("Git Version Control Demo");
+        System.out.println("Student: Dhanunjay");
+        
     }
 }
